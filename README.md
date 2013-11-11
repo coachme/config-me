5276d0293c3d7fe0eee2477df3b7e3d35ef09c4c
Ricardo Graça <ricardo@devius.net>
2013-11-11 17:14:04 +0000

    Import into version control

---
+++ README.md
@@ -0,0 +1,39 @@
+Config-Me
+=========
+Simple configuration module for node.js
+
+---
+```js
+var config = require('config-me');
+
+config.set('setting', 'on');
+config.get('setting'); // 'on'
+
+```
+or
+```js
+// your_app_root/config/db.js
+
+module.exports = {
+  user: 'dbUser',
+  password: 'safePassword',
+  database: 'your_app_db',
+  host: 'localhost',
+  port: 5432
+}
+
+//your_app_root/index.js
+
+var config = require('config-me').init(__dirname + '/config');
+var dbConfig = config.get('db');
+var dbPort = dbConfig.port; // 5432
+
+```
+## About
+Parts of this are taken directly from Express, since I'm lazy and Express is beautifully written.  All I wanted was a
+simple central configuration module. I did some modifications to the code so that it fits my use case better, e.g.
+reading config files from a directory.
+
+It can read a config directory, load all .js files in there and attach the objects found to the main config module.
+It doesn't enforce a configuration format for the files, but it's expected that these are objects or arrays. Base
+properties can be retrieved with the get method and changed with the set method.
---
+++ index.js
@@ -0,0 +1,55 @@
+/**
+ * Config-Me simple configuration module for node.js
+ *
+ * Parts of this are taken directly from Express with some modifications.
+ *
+ * It can read a config directory, load all .js files in there and attach the objects found to the main config module.
+ * It doesn't enforce a configuration format for the files, but it's expected that these are objects or arrays. Base
+ * properties can be retrieved with the get method and changed with the set method.
+ *
+ * @module config-me
+ * @author Ricardo Graça <ricardo@devius.net>
+ */
+var basename = require('path').basename
+var fs = require('fs')
+var currentEnvironment = process.env.ENV_VARIABLE || 'development'
+var config = {
+  settings: {},
+
+  // Read all the files from path and add any configuration objects to the config object
+  init: function(path) {
+    if (typeof path !== 'string') throw new Error('Config-Me init function requires a string as first argument')
+
+    fs.readdirSync(path).forEach(function(filename) {
+      if (!/\.js$/.test(filename)) return false
+
+      var name = basename(filename, '.js')
+      config.settings[name] = require([path, '/', filename].join(''))
+    })
+    
+    return this
+  },
+
+  configure: function(env, fn) {
+    var envs = 'all'
+    var args = [].slice.call(arguments) // Convert the arguments variable into a true array
+
+    fn = args.pop()
+
+    if (args.length) envs = args
+    if (envs === 'all' || ~envs.indexOf(currentEnvironment)) fn.call(this)
+
+    return this
+  },
+
+  set: function(setting, val) {
+    this.settings[setting] = val;
+    return this
+  },
+
+  get: function(setting) {
+    return this.settings[setting]
+  }
+}
+
+module.exports = config
